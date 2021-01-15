ca3ff7dc3db9823a5018ff15a90d5e616e8f3e6d
twinstae <rabolution@gmail.com>
2021-01-15 18:54:02 +0900

    update urls.py using viewset

--- realworld/apps/quickstart/urls.py
+++ realworld/apps/quickstart/urls.py
@@ -2,23 +2,44 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 # API endpoints
-from realworld.apps.quickstart import views
+from realworld.apps.quickstart.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
+    path('', api_root),
     path('snippets/',
-         views.SnippetList.as_view(),
+         snippet_list,
          name='snippet-list'),
     path('snippets/<int:pk>/',
-         views.SnippetDetail.as_view(),
+         snippet_detail,
          name='snippet-detail'),
     path('snippets/<int:pk>/highlight/',
-         views.SnippetHighlight.as_view(),
+         snippet_highlight,
          name='snippet-highlight'),
     path('users/',
-         views.UserList.as_view(),
+         user_list,
          name='user-list'),
     path('users/<int:pk>/',
-         views.UserDetail.as_view(),
+         user_detail,
          name='user-detail')
 ])
