6e6fb60789ed8914bc44cfd91859bfc4a55e442c
twinstae <rabolution@gmail.com>
2021-01-15 14:19:23 +0900

    create Snippet test

--- realworld/apps/quickstart/test_user.py
+++ realworld/apps/quickstart/test_user.py
@@ -6,6 +6,24 @@ from realworld.secret import ADMIN_PASSWORD
 
 class UserTest(APITestCase):
     client = APIClient()
+    expected = [
+        {
+            "id": 1,
+            "title": "",
+            "code": "foo = \"bar\"\n",
+            "linenos": False,
+            "language": "python",
+            "style": "friendly"
+        },
+        {
+            "id": 2,
+            "title": "",
+            "code": "print(\"hello, world\")\n",
+            "linenos": False,
+            "language": "python",
+            "style": "friendly"
+        }
+    ]
 
     @classmethod
     def setUp(cls):
@@ -14,6 +32,25 @@ class UserTest(APITestCase):
             password=ADMIN_PASSWORD,
         )
 
+    @classmethod
+    def setUpTestData(cls):
+
+        snippet = Snippet(code=cls.expected[0]['code'])
+        snippet.save()
+
+        snippet = Snippet(code=cls.expected[1]['code'])
+        snippet.save()
+
     def test_get_users_list(self):
         response = self.client.get('/users/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_get_snippet_list(self):
+        response = self.client.get('/snippets/')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == self.expected
+
+    def test_get_snippet_2(self):
+        response = self.client.get('/snippets/2')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == self.expected[1]
