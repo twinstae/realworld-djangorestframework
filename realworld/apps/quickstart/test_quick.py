b80541cd76201b48c3ba9782ed4f6e668b921392
twinstae <rabolution@gmail.com>
2021-01-15 23:25:49 +0900

    test_quick 정리

--- realworld/apps/quickstart/test_quick.py
+++ realworld/apps/quickstart/test_quick.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponse
 from rest_framework import status
 from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from rest_framework.test import APITestCase, APIClient
 
 from realworld.apps.quickstart.models import Snippet
-from realworld.apps.quickstart.serializers import SnippetSerializer
+
 
 expected = [{'code': 'foo = "bar",',
              'highlight': 'http://testserver/snippets/1/highlight/',
@@ -71,29 +70,29 @@ class QuickTest(APITestCase):
         cls.snippet_2.save()
 
     def test_get_users_list(self):
-        response = self.client.get(
-            SNIPPETS_LIST
-        )
+        response = self.client.get(SNIPPETS_LIST)
         assert response.status_code == status.HTTP_200_OK
 
     def test_get_snippet_list_wrong_url(self):
-        response = self.client.get(
-            SNIPPETS_LIST[:-1]
-        )
+        response = self.client.get(SNIPPETS_LIST[:-1])
         assert response.status_code == status.HTTP_301_MOVED_PERMANENTLY
 
     def test_get_snippet_list(self):
-        response = self.client.get(
-            SNIPPETS_LIST
-        )
+        response = self.client.get(SNIPPETS_LIST)
         assert response.status_code == status.HTTP_200_OK
         assert parse_body(response)['results'] == expected
 
-    def test_create_snippet(self):
+    def test_create_snippet_without_login(self):
+        create = {'code': 'new idea'}
+        response = self.client.post(SNIPPETS_LIST,
+                                    create, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_create_snippet_with_login(self):
         self.client.force_login(user=self.user)
-        create = {
-            'code': 'new idea',
-        }
+        create = {'code': 'new idea'}
+        response = self.client.post(SNIPPETS_LIST,
+                                    create, format='json')
         expected_create = {
             'code': 'new idea',
             'highlight': 'http://testserver/snippets/3/highlight/',
@@ -105,54 +104,34 @@ class QuickTest(APITestCase):
             'title': '',
             'url': 'http://testserver/snippets/3/'
         }
-        response = self.client.post(
-            SNIPPETS_LIST,
-            create,
-            format='json'
-        )
         assert response.status_code == status.HTTP_201_CREATED
         assert parse_body(response) == expected_create
 
-    def test_update_snippet(self):
+    def test_update_snippet_with_login(self):
         self.client.force_login(user=self.user)
-        update = {
-            'code': "what can i do now!"
-        }
-        response = self.client.put(
-            SNIPPETS_DETAIL,
-            update,
-            format='json'
-        )
-        expected_update = {'code': 'what can i do now!',
-                           'highlight': 'http://testserver/snippets/2/highlight/',
-                           'id': 2,
-                           'language': 'python',
-                           'linenos': False,
-                           'owner': 'stelo',
-                           'style': 'friendly',
-                           'title': '',
-                           'url': 'http://testserver/snippets/2/'}
+        update = {'code': "what can i do now!"}
+        response = self.client.put(SNIPPETS_DETAIL,
+                                   update, format='json')
         assert response.status_code == status.HTTP_200_OK
-        assert parse_body(response) == expected_update
+        assert parse_body(response)['code'] == 'what can i do now!'
 
-    def test_delete_snippet(self):
+    def test_update_snippet_without_login(self):
+        update = {'code': "what can i do now!"}
+        response = self.client.put(SNIPPETS_DETAIL,
+                                   update, format='json')
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_snippet_with_login(self):
         self.client.force_login(user=self.user)
-
-        response = self.client.delete(
-            SNIPPETS_DETAIL
-        )
+        response = self.client.delete(SNIPPETS_DETAIL)
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
     def test_delete_snippet_without_login(self):
-        response = self.client.delete(
-            SNIPPETS_DETAIL
-        )
+        response = self.client.delete(SNIPPETS_DETAIL)
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_get_snippet_2(self):
-        response = self.client.get(
-            SNIPPETS_DETAIL
-        )
+        response = self.client.get(SNIPPETS_DETAIL)
         content_expected = expected[1]
         assert response.status_code == status.HTTP_200_OK
         assert parse_body(response) == content_expected
